9d449d8de3f588469e9a06f123bb95b97f6cd830
arun980798 <arunrajput88176894@gmail.com>
2025-04-05 02:00:42 +0530

    //

--- my-pro/src/App.jsx
+++ my-pro/src/App.jsx
@@ -3,34 +3,31 @@ import React from 'react'
 
 function App() {
 
-   let [ counter, setCounter]=  useState(12)
-
- 
-
-  const addValue =() =>{
-    console.log("clicked ", counter);
-    counter= counter+1 
-    setCounter(counter)
-  }
-
-  const removeValue =() =>{
-    console.log("clicked ", counter);
-    counter= counter-1
-    setCounter(counter)
-  }
-
+  
 
 
 
   return (
-    <div className=' bg-black grid justify-center h-screen align-middle text-white items-center'>
-      <h1> counter {counter} </h1>
-      <button className=' border-2 border-amber-200 rounded-2xl ' onClick={addValue}> add value {counter} </button>
-      <button onClick={removeValue}> remove this line  {counter} </button>
-      <h1>footer {counter} </h1>
-      
-      
+  <>
+    <div className=' bg-black h-screen w-full  '>
+        <div className=' bg-white align-bottom mt-12 h-6 flex '>
+          <div>white    </div><br />
+          <div>black    </div>
+
+
+        </div>
+
     </div>
+          
+          
+          
+          
+          
+  </>
+
+
+
+
   )
 }
 
