0a2e096a450424f03c6f370f6f2ddd9cdc36cffb
arun980798 <arunrajput88176894@gmail.com>
2025-04-15 00:18:21 +0530

    complet login page with backhand  work

--- my-pro/src/component/auth/login.jsx
+++ my-pro/src/component/auth/login.jsx
@@ -5,18 +5,13 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   
-
   const sumbitHandler =(e)=>{
-    e.preventDefault
-    console.log( "hello guys form sumbit ")
-   
-    
-
+    e.preventDefault()
+    console.log( "email is " ,email)
+    console.log( "password is ", password)
+    setEmail("")
+    setPassword("")
   }
-
-
-
-
 
 
   return (
@@ -25,11 +20,24 @@ const Login = () => {
       <div className='border-2 border-emerald-600  rounded-2xl'>
           <form 
           onSubmit={(e)=>{
-                    sumbitHandler(e)
+               sumbitHandler(e)
           }}
           action="" className='flex flex-col items-center justify-center text-white p-20 '> 
-            <input required className=' border-2 border-emerald-600 py-3 text-xl outline-none bg-transparent px-5 rounded-full placeholder:text-white' type="email" placeholder='enter your email ' />
-            <input required className=' border-2 border-emerald-600 py-3 text-xl outline-none bg-transparent px-5 rounded-full placeholder:text-white mt-10' type="password" placeholder='enter your password ' />
+            <input
+            value={email}
+            onChange={(e)=>{
+              setEmail(e.target.value)
+
+            }}
+             required
+              className=' border-2 border-emerald-600 py-3 text-xl outline-none bg-transparent px-5 rounded-full placeholder:text-white' type="email" placeholder='enter your email ' />
+            <input 
+            value={password}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+            required
+             className=' border-2 border-emerald-600 py-3 text-xl outline-none bg-transparent px-5 rounded-full placeholder:text-white mt-10' type="password" placeholder='enter your password ' />
             <button className=' mt-7 border-2 py-3 text-xl outline-none  px-5 rounded-full  bg-blue-400'> login </button>
           </form>
       </div>
